82258fa47ca5857e3508f219c51ced9a0de4f3d5
Ossian Eriksson <ossian.eriksson@elev.ga.ntig.se>
2022-08-31 14:08:36 +0200

    Added Namespace Algo
    Namespace Algo includes print function

--- readandwrite/src/Main.cpp
+++ readandwrite/src/Main.cpp
@@ -1,4 +1,19 @@
+#include <iostream>
+
+
+namespace Algo
+{
+	template <typename T> 
+	//Print and makes new row. 
+	void Print(T t)
+	{
+		std::cout << number << "\n";
+	}
+}
+
+
+
 int main()
 {
-
+	Algo::Print(1450);
 }
